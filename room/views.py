c22f3528916fe51851a8f0f3f8c3a6db842346cb
AbhishekVarma11 <abhishekgollapalli11@gmail.com>
2023-06-23 17:48:35 +0530

    added room functionality

--- room/views.py
+++ room/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Room
+@login_required
+def rooms(request):
+    rooms=Room.objects.all()
+    
+    return render(request,'room/rooms.html',{'rooms':rooms})
 
-# Create your views here.
+@login_required
+def room(request,slug):
+    room=Room.objects.get(slug=slug)
+    
+    return render(request,'room/room.html',{'room':room})
